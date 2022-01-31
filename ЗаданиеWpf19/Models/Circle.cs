5562bed4281fdcc8ab32ae18149cb72e139195dc
Aleksandr Panarin <panarinalex12@gmail.com>
2022-01-31 16:29:50 +0300

    Добавьте файлы проекта.

---
+++ ЗаданиеWpf19/Models/Circle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЗаданиеWpf19.Models
+{
+    class Circle
+    {
+        public static double Add(double r) => (2 * Math.PI) * r;
+    }
+}
---
+++ ЗаданиеWpf19/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ЗаданиеWpf19.Models;
+
+namespace ЗаданиеWpf19.ViewModels
+{
+    class MainWindowsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number3 = Circle.Add(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            return true;
+        }
+        public MainWindowsViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
